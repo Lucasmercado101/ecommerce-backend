39303d45bf9a3bf86f719cbdc4e744ccf5f9311f
Lucasmercado101 <lucasmercado101@gmail.com>
2023-02-05 13:26:58 -0300

    can get all products

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,19 @@ const createProduct: RequestHandler = (req, res) => {
       res.send(err);
     });
 };
+const getAllProducts: RequestHandler = (req, res) => {
+  prisma.product
+    .findMany()
+    .then((product) => {
+      res.send(product);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+};
 
 app.post("/initial", createProduct);
+app.get("/getAllProducts", getAllProducts);
 
 app
   .listen(PORT, () => {
